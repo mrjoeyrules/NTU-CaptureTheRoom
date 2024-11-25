761faa8c6eca63958fcee9a03c83dc237ef648c0
mrjoeyrules <mrjoeyrules@gmail.com>
2024-11-25 15:53:31 +0000

    Initial Commit

---
+++ NTU CaptureTheRoomTests/NTU_CaptureTheRoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  NTU_CaptureTheRoomTests.swift
+//  NTU CaptureTheRoomTests
+//
+//  Created by Joseph Cuesta Acevedo on 25/11/2024.
+//
+
+import Testing
+@testable import NTU_CaptureTheRoom
+
+struct NTU_CaptureTheRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
