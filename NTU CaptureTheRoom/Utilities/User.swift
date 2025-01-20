d623fe9341a1fa0a31022812310d75a3fbb22e11
mrjoeyrules <mrjoeyrules@gmail.com>
2025-01-20 17:33:21 +0000

    Fully working login and username system, next is team selection

---
+++ NTU CaptureTheRoom/Utilities/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  NTU CaptureTheRoom
+//
+//  Created by Joseph Cuesta Acevedo on 13/01/2025.
+//
+
+import Foundation
---
+++ NTU CaptureTheRoom/Views/FirstUserInfo.swift
@@ -0,0 +1,8 @@
+//
+//  FirstUserInfo.swift
+//  NTU CaptureTheRoom
+//
+//  Created by Joseph Cuesta Acevedo on 20/01/2025.
+//
+
+import Foundation
